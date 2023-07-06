eaf888b84021e33df13d70de18d5bef8f96888f2
Yousef-El-Black <yousefelblackdev@gmail.com>
2023-07-06 20:00:31 +0300

    Start Up API Project

---
+++ api/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    img: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
