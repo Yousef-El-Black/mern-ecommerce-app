428483329c5decde9f0532ab1a8272299f1f988c
Yousef-El-Black <yousefelblackdev@gmail.com>
2023-07-30 18:58:16 +0300

    Update

--- api/src/utils/connectDB.ts
+++ api/src/utils/connectDB.ts
@@ -4,7 +4,7 @@ import { MONGO_URL_ATLAS, MONGO_URL_LOCAL } from "../config";
 export const connectDB = async (): Promise<void> => {
   await mongoose
     // .connect(MONGO_URL_ATLAS as string)
-    .connect(MONGO_URL_LOCAL as string)
+    .connect(MONGO_URL_ATLAS as string)
     .then(() => console.log("DB Connection Successful!"))
     .catch((err) => console.log(err));
 };
