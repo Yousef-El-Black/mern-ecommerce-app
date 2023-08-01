ed45d3d68e3c855938ec07b3d76a3494f85bb45e
Yousef-El-Black <yousefelblackdev@gmail.com>
2023-08-01 21:13:07 +0300

    Update

--- admin/environment/environment.ts
+++ admin/environment/environment.ts
@@ -4,11 +4,8 @@ let apiUrl: string;
 
 if (ENV === "dev") {
   apiUrl = LOCAL_API_URL?.toString() as string;
-} else if (ENV === "prod") {
-  apiUrl = API_URL?.toString() as string;
 } else {
-  apiUrl = "http://localhost:8080/";
+  apiUrl = API_URL?.toString() as string;
 }
-apiUrl = "http://localhost:8080/";
 
 export { apiUrl };
